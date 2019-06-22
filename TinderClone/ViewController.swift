1b9ea7e0228b26f4d1f892216066fd79a50d098f
Goodnews <krgoodnews@gmail.com>
2019-06-23 01:08:12 +0900

    Init project

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by 국윤수 on 23/06/2019.
+//  Copyright © 2019 국윤수. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
