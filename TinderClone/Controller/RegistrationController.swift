9c054667b769303605739b314a70db3ade1139be
Goodnews <krgoodnews@gmail.com>
2019-07-01 08:55:54 +0900

    Size class Landscape Layout

--- TinderClone/Controller/RegistrationController.swift
+++ TinderClone/Controller/RegistrationController.swift
@@ -85,35 +85,65 @@ class RegistrationController: UIViewController {
   }
 
   @objc private func handleKeyboardShow(notification: Notification) {
+    print("notification.userInfo = \(notification.userInfo)")
     guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
     
     let keyboardFrame = value.cgRectValue
 
     // figure out how tall the gap is from the register button to the bottom of the screen
-    let bottomSpace = view.frame.height - stackView.frame.origin.y - stackView.frame.height
+    let bottomSpace = view.frame.height - overallStackView.frame.origin.y - overallStackView.frame.height
 
     let difference = keyboardFrame.height - bottomSpace
     self.view.transform = CGAffineTransform(translationX: 0, y: -difference - 8)
   }
 
-  lazy var stackView = UIStackView(arrangedSubviews: [
+  lazy var verticalStackView: UIStackView = {
+    let sv = UIStackView(arrangedSubviews: [
+      fullNameTextField,
+      emailTextField,
+      passwordTextField,
+      registerButton
+    ])
+    sv.axis = .vertical
+    sv.distribution = .fillEqually
+    sv.spacing = 8
+    return sv
+  }()
+
+  lazy var overallStackView = UIStackView(arrangedSubviews: [
     selectPhotoButton,
-    fullNameTextField,
-    emailTextField,
-    passwordTextField,
-    registerButton
+    verticalStackView
   ])
 
+  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+    super.traitCollectionDidChange(previousTraitCollection)
+    if self.traitCollection.verticalSizeClass == .compact {
+      overallStackView.axis = .horizontal
+    } else {
+      overallStackView.axis = .vertical
+    }
+  }
+
   private func setupLayout() {
-    view.addSubview(stackView)
-    stackView.axis = .vertical
-    stackView.spacing = 8
-    stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 32, bottom: 0, right: 32))
-    stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    view.addSubview(overallStackView)
+
+    overallStackView.axis = .horizontal
+
+    selectPhotoButton.widthAnchor.constraint(equalToConstant: 275).isActive = true
+    overallStackView.spacing = 8
+    overallStackView.anchor(top: nil, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 32, bottom: 0, right: 32))
+    overallStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
+
+  private let gradientLayer = CAGradientLayer()
+
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+
+    gradientLayer.frame = view.bounds
   }
 
   private func setupGradientLayer() {
-    let gradientLayer = CAGradientLayer()
     let topColor = UIColor.rgb(red: 253, green: 91, blue: 95)
     let bottomColor = UIColor.rgb(red: 229, green: 0, blue: 114)
     gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
